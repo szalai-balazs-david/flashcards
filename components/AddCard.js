f9d8580ceda7925dd34a6ed552a383dfda7b7c9f
Balazs <balazs@altaeros.com>
2020-11-01 15:12:32 -0500

    Add basic style to AddCard form

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { StyleSheet, Text, View, TextInput, Alert, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import {useForm} from 'react-hook-form'
 import {addCard} from '../utils/storage'
 
@@ -27,17 +27,19 @@ export default function AddCard({navigation, route}) {
         onChangeText={text => {
           setValue("question", text)
         }}
+        style={styles.inputField}
       />
       <Text>Answer</Text>
       <TextInput 
         onChangeText={text => {
           setValue("answer", text)
         }}
+        style={styles.inputField}
       />
       <TouchableOpacity
         onPress={handleSubmit(onSubmit)}
       >
-        <Text>
+        <Text style={styles.submitButton}>
           Submit
         </Text>
       </TouchableOpacity>
@@ -52,4 +54,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputField:{
+    width: '80%',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    fontSize: 20,
+    borderColor: 'black',
+  },
+  submitButton: {
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    width: 200,
+    height: 70,
+    borderWidth: 1,
+    borderRadius: 5,
+    fontSize: 20,
+    borderColor: 'black',
+    backgroundColor: 'black',
+    color: 'white'
+  }
 });
